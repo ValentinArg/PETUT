6dd5b7bb174666709329521559b46c1a2e9a3d0f
fbremec <fbremec@gmail.com>
2016-09-29 10:18:08 +0200

    import nouveau seveur

--- PETUT/src/Servlets/Index.java
+++ PETUT/src/Servlets/Index.java
@@ -1,5 +1,17 @@
 package Servlets;
 
-public class Index {
+import java.io.IOException;
 
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Index extends HttpServlet{
+	
+	private String VUE = "/WEB-INF/JSP/index.jsp";
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+		
+		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );//envoie la requete et la reponse au JSP specifier en url
+		
+	}
 }
