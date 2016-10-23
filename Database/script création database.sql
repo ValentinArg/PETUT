a5ea0fc18878950a3e85dda3fb3b2d298553e803
fbremec <fbremec@gmail.com>
2016-10-23 21:33:20 +0200

    modif scipt génération base

--- Database/script création database.sql
+++ Database/script création database.sql
@@ -6,9 +6,11 @@ create table Utilisateur(
     Nom varchar(30) NOT NULL,
     Prenom varchar(30) NOT NULL,
     Adresse varchar(100),
+    Code_Postal int,
+    Ville varchar(50),
     Photo varchar(500),
     Telephone int,
-    AdresseMail varchar(50) NOT NULL check(AdresseMail like "%@%"),
+    Adresse_Mail varchar(50) NOT NULL check(AdresseMail like "%@%"),
     Type_Utilisateur varchar(10) NOT NULL check(Type_Utilisateur="enseignant" || Type_Utilisateur="etudiant")
 );
 
@@ -16,7 +18,9 @@ create table Utilisateur(
 create table Enseigne(
 	id_Enseigne int PRIMARY KEY,
     Nom varchar(30) NOT NULL,
-    Adresse varchar(100) NOT NULL
+    Adresse varchar(100) NOT NULL,
+    Code_Postal int NOT NULL,
+    Ville varchar(50) NOT NULL
 );
 
 create table UtilisateurEnseigne(
@@ -106,7 +110,7 @@ create table Reponse(
     id_Commentaire int NOT NULL,
     Identifiant varchar(30) NOT NULL,
     Texte varchar(10000) NOT NULL,
-    date_Reponse char(10) NOT NULL check(date_Reponse like "%%/%%/%%%%"),
+    Date_Reponse char(10) NOT NULL check(date_Reponse like "%%/%%/%%%%"),
     FOREIGN KEY(id_Commentaire) REFERENCES Commentaire(id_Commentaire),
     FOREIGN KEY(Identifiant) REFERENCES Utilisateur(Identifiant)
 );
@@ -119,7 +123,7 @@ create table Document(
     Lien varchar(500) NOT NULL,
     date_Document char(10) NOT NULL check(date_Document like "%%/%%/%%%%"),
     Nom varchar(50),
-    type_Document varchar(50) NOT NULL,
+    Type_Document varchar(50) NOT NULL,
     FOREIGN KEY(id_Commentaire) REFERENCES Commentaire(id_Commentaire),
     FOREIGN KEY(id_Reponse) REFERENCES Reponse(id_Reponse),
     FOREIGN KEY(Identifiant) REFERENCES Utilisateur(Identifiant)
@@ -128,9 +132,22 @@ create table Document(
 create table DocumentSujet(
 	id_Document int NOT NULL,
     id_Sujet int NOT NULL,
-    type_Document varchar(30) NOT NULL check(type_Document="sujet" || type_Document="reponse" || type_Document="docimporte"),
+    Type_Document varchar(30) NOT NULL check(type_Document="sujet" || type_Document="reponse" || type_Document="docimporte"),
     FOREIGN KEY(id_Document) REFERENCES Document(id_Document),
     FOREIGN KEY(id_Sujet) REFERENCES Sujet(id_Sujet),
 	PRIMARY KEY(id_Document, id_Sujet)
-)
+);
+
+create table Evenement(
+	id_Evenement int NOT NULL,
+    Nom varchar(50) NOT NULL,
+    Adresse varchar(50) NOT NULL,
+    Code_Postal int NOT NULL,
+    Vile varchar(50) NOT NULL,
+    Date_Evenement char(10) NOT NULL check(date_Reponse like "%%/%%/%%%%"),
+    Heure char(5) NOT NULL check(Heure like "%%H%%"),
+    Type_Evenement varchar(30) check(Type_Forum = "viescolaire" || Type_Forum = "vieetudiante"),
+    id_Enseigne int NOT NULL,
+    FOREIGN KEY(id_Enseigne) REFERENCES Enseigne(id_Enseigne)
+);
     
--- PETUT/src/Beans/Enseigne.java
+++ PETUT/src/Beans/Enseigne.java
@@ -2,6 +2,10 @@ package Beans;
 
 public class Enseigne {
 	
+	private int id;
+	private String nom;
+	private String adresse;
+	
 
 
 
