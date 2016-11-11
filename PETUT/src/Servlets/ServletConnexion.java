6e5da2771cdc27a38ddd9264e9901af5d075b8e8
asus <asus@asuspc>
2016-11-11 13:25:44 +0100

    checkbox et cookie "se souvenir de moi"

--- PETUT/src/Servlets/ServletConnexion.java
+++ PETUT/src/Servlets/ServletConnexion.java
@@ -3,6 +3,7 @@ package Servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,10 +16,19 @@ public class ServletConnexion extends HttpServlet{
 
 	private String VUE = "/WEB-INF/jsp/connexion.jsp";
 	private String REDIRECT = "/PE2I/restreint/accueil";
+	private String CHAMP_SOUVENIR = "souvenir";
+	private String COOKIE_SOUVENIR = "souvenir";
+	private int COOKIE_MAX_AGE = 60 * 60 * 24 * 365;
 
 	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 		
-		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );//envoie la requete et la reponse au JSP specifier en url
+		
+		String seSouvenir = getCookieValue( request, COOKIE_SOUVENIR );
+		if(seSouvenir != null && seSouvenir.equals("oui")){
+			response.sendRedirect(REDIRECT);
+		}else{
+			this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );//envoie la requete et la reponse au JSP specifier en url
+		}
 		
 	}
 	
@@ -30,6 +40,13 @@ public class ServletConnexion extends HttpServlet{
 			String identifiant = form.connexionUtilisateur(request);
 			HttpSession session = request.getSession();
 			session.setAttribute("identifiant", identifiant );
+			if ( request.getParameter( CHAMP_SOUVENIR ) != null ) {
+				String seSouvenir="oui";
+				setCookie( response, COOKIE_SOUVENIR, seSouvenir, COOKIE_MAX_AGE );
+			} else {
+		        /* Demande de suppression du cookie du navigateur */
+		        setCookie( response, COOKIE_SOUVENIR, "", 0 );
+		    }
         	response.sendRedirect(REDIRECT);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -38,5 +55,31 @@ public class ServletConnexion extends HttpServlet{
 		}
 	}
 	
+
+    /**
+     * Méthode utilitaire gérant la récupération de la valeur d'un cookie donné
+     * depuis la requête HTTP.
+     */
+    private static String getCookieValue( HttpServletRequest request, String nom ) {
+        Cookie[] cookies = request.getCookies();
+        if ( cookies != null ) {
+            for ( Cookie cookie : cookies ) {
+                if ( cookie != null && nom.equals( cookie.getName() ) ) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+    
+    /*
+     * Méthode utilitaire gérant la création d'un cookie et son ajout à la
+     * réponse HTTP.
+     */
+    private static void setCookie( HttpServletResponse response, String nom, String valeur, int maxAge ) {
+        Cookie cookie = new Cookie( nom, valeur );
+        cookie.setMaxAge( maxAge );
+        response.addCookie( cookie );
+    }
 	
 }
--- PETUT/src/Servlets/ServletDeconnexion.java
+++ PETUT/src/Servlets/ServletDeconnexion.java
@@ -3,6 +3,7 @@ package Servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,14 +11,45 @@ import javax.servlet.http.HttpSession;
 
 public class ServletDeconnexion extends HttpServlet {
 	   public static final String URL_REDIRECTION = "/PE2I/connexion";
+	   private static final String COOKIE_SOUVENIR = "souvenir";
 
 	   public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 	        /* Récupération et destruction de la session en cours */
 	        HttpSession session = request.getSession();
+	        String seSouvenir = getCookieValue( request, COOKIE_SOUVENIR );
+			if(seSouvenir != null && seSouvenir.equals("oui")){
+				setCookie( response, COOKIE_SOUVENIR, "", 0 );
+			}
 	        session.invalidate();
 
 	        /* Redirection vers la page de connexion ! */
 	        response.sendRedirect( URL_REDIRECTION );
 	   }
+	   
+	   /**
+	     * Méthode utilitaire gérant la récupération de la valeur d'un cookie donné
+	     * depuis la requête HTTP.
+	     */
+	    private static String getCookieValue( HttpServletRequest request, String nom ) {
+	        Cookie[] cookies = request.getCookies();
+	        if ( cookies != null ) {
+	            for ( Cookie cookie : cookies ) {
+	                if ( cookie != null && nom.equals( cookie.getName() ) ) {
+	                    return cookie.getValue();
+	                }
+	            }
+	        }
+	        return null;
+	    }
+	    
+	    /*
+	     * Méthode utilitaire gérant la création d'un cookie et son ajout à la
+	     * réponse HTTP.
+	     */
+	    private static void setCookie( HttpServletResponse response, String nom, String valeur, int maxAge ) {
+	        Cookie cookie = new Cookie( nom, valeur );
+	        cookie.setMaxAge( maxAge );
+	        response.addCookie( cookie );
+	    }
 }
 
