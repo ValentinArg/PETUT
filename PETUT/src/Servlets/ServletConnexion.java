29665d3f9b3e2669b81d5ef8bfa531fb86e97b0a
fbremec <fbremec@gmail.com>
2016-11-12 15:59:51 +0100

    Update ServletConnexion.java

--- PETUT/src/Servlets/ServletConnexion.java
+++ PETUT/src/Servlets/ServletConnexion.java
@@ -17,7 +17,7 @@ public class ServletConnexion extends HttpServlet{
 	private String VUE = "/WEB-INF/jsp/connexion.jsp";
 	private String REDIRECT = "/PE2I/restreint/accueil";
 	private String CHAMP_SOUVENIR = "souvenir";
-	//methode Get
+	//methode Get sur gitHub
 	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 		
 		HttpSession session = request.getSession();
