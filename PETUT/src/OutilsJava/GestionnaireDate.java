33620424f71a9ef92353758e893e1083765733dc
fbremec <fbremec@gmail.com>
2016-11-22 22:04:22 +0100

    dynamique terminer manque plus que les tests et les modifs par rapport au tests

---
+++ PETUT/src/OutilsJava/GestionnaireDate.java
@@ -0,0 +1,43 @@
+package OutilsJava;
+
+import java.util.HashMap;
+
+public class GestionnaireDate {
+	
+	private static final String[] mois = {"Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"};
+	
+	// convertir une chaine date de type 00/00/0000 en chaine de type 00 mois 0000
+	public static String convertDateToString(String date){
+		String retour = "";
+		String jour = "";
+		String mois = ""; 
+		String annee = "";
+		int compteur = 0;
+		while(date.charAt(compteur)!='/'){
+			jour+=date.charAt(compteur);
+			compteur++;
+		}
+		compteur++;
+		while(date.charAt(compteur)!='/'){
+			mois+=date.charAt(compteur);
+			compteur++;
+		}
+		compteur++;
+		while(date.charAt(compteur)!='/' && compteur!=9){
+			annee+=date.charAt(compteur);
+			if(compteur<9)
+				compteur++;
+		}
+		annee+=date.charAt(compteur);
+		int numeroMois = (Integer.parseInt(mois)-1);
+		mois = GestionnaireDate.mois[numeroMois];
+		return jour+" "+mois+" "+annee;
+		
+	}
+	
+	
+	
+	
+	
+
+}
